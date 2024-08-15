541aec6e6537eaf28a1185bb4b37400f5b94481a
jbalooshie <jhboshier@gmail.com>
2024-08-15 15:39:31 -0400

    Add random logic to determine computer's choice

--- app.py
+++ app.py
@@ -1,28 +1,34 @@
+import random
+
 def main_menu():
-    while True:
-        print('Hello! Welcome to rock, paper, scissors. Type "New" to start a new game. Type "Exit" to exit.')
-        menu_choice = input()
-        if menu_choice == 'New':
-            play_game()
-        elif menu_choice == 'Exit':
-            return print('Goodbye')
-        else:
-            print('Invalid selection. Please choose "New" or "Exit".')
+    status = ''
+    while status != 'Exit':
+        print('Hello! Welcome to rock, paper, scissors.')
+        status = play_game()
 
 def play_game():
     selection = ''
     while selection != 'Exit':
         selection = input('Please make your selection.')
-        cpu_selection = 'Rock'
-        if selection == 'Paper':
+        choices = ('Rock', 'Paper', 'Scissors')
+        cpu_selection = choices[random.randint(0,2)]
+        if selection == cpu_selection:
+            print(f'Both chose {selection}. Tie!')
+        elif selection == 'Scissors' and cpu_selection == 'Paper':
+            print('Scissors cuts Paper. You win!')
+        elif selection == 'Paper' and cpu_selection == 'Rock':
             print('Paper covers Rock. You win!')
-        elif selection == 'Rock':
-            print('Both chose Rock. Tie!')
-        elif selection == 'Scissors':
-            print('Rock beats scissors. You lose!')
+        elif selection == 'Rock' and cpu_selection == 'Scissors':
+            print('Rock cruches scissors. You win!')
+        elif selection == 'Scissors' and cpu_selection == 'Rock':
+            print('Rock crushes scissors. Computer wins!')
+        elif selection == 'Paper' and cpu_selection == 'Scissors':
+            print('Scissors cuts Paper. Computer wins!')
+        elif selection == 'Rock' and cpu_selection == 'Paper':
+            print('Paper covers Rock. Computer wins!')
         elif selection == 'Exit':
             print('Thanks for playing!')
-            return False
+            return 'Exit'
         else:
             print("Invalid selection. Please choose Rock, Paper, or Scissors.")
 
